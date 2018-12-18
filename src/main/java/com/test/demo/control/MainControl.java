fe71383cec664ab7c91d54496670c2aca16fe1e2
Tester <894799178@qq.com>
2018-12-18 19:36:38 +0800

    构建DAO层(未测试) 采用mybatis, 数据库使用 mysql

--- src/main/java/com/test/demo/control/MainControl.java
+++ src/main/java/com/test/demo/control/MainControl.java
@@ -2,6 +2,7 @@ package com.test.demo.control;
 
 
 import com.test.demo.model.DamageBean;
+import com.test.demo.model.DamageData;
 import com.test.demo.service.ChromeSeleniumService;
 import com.test.demo.service.PlayerService;
 import org.openqa.selenium.WebDriver;
@@ -9,10 +10,11 @@ import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainControl {
@@ -37,6 +39,13 @@ public class MainControl {
         String damageData = chromeSeleniumService.getDamegeDataBeans(instance);
        System.out.println("控制层:"+damageData);
         return damageData;
+    }
+    @PostMapping(value = "/updateData")
+    @ResponseBody
+    public void updateData(@RequestParam(value = "datas[]")  List<String> datas){
+
+
+
     }
 
 
